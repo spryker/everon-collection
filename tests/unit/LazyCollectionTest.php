8cb8d010a510ba05f495cc2192d91f27341eb229
Oliwier Ptak <oliwierptak@gmail.com>
2015-12-11 19:57:23 +0100

    cleanups

--- tests/unit/LazyCollectionTest.php
+++ tests/unit/LazyCollectionTest.php
@@ -1,9 +1,9 @@
 <?php
 
 /**
- * This file is part of the Everon framework.
+ * This file is part of the Everon components.
  *
- * (c) Oliwier Ptak <EveronFramework@gmail.com>
+ * (c) Oliwier Ptak <everonphp@gmail.com>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
